25a0aa94e7903a109f7112c828bbb11b82630d04
Joar <joar1.kodehode@gmail.com>
2022-07-06 13:48:50 +0200

    pimp

--- src/app.js
+++ src/app.js
@@ -7,27 +7,29 @@ export const Calculator = () => {
   return (
     <div className='calc-body'>
       <div className='header'>Sommeroppgave kalkulator</div>
-      <input className='skjerm' placeholder='0'/>
-        <div className='prev-op'></div>
-        <div className='current-op'></div>
+      {/* <input className='skjerm' placeholder='0'/> */}
+      <div className='screen'>
+        <div className='prev-op'>1234</div>
+        <div className='current-op'>1234</div>
+      </div>
 
-      <div className='tastatur'>
-        <button className='knapp'>7</button>
-        <button className='knapp'>8</button>
-        <button className='knapp'>9</button>
-        <button className='knapp'>DEL</button>
-        <button className='knapp'>4</button>
-        <button className='knapp'>5</button>
-        <button className='knapp'>6</button>
-        <button className='knapp'>+</button>
-        <button className='knapp'>1</button>
-        <button className='knapp'>2</button>
-        <button className='knapp'>3</button>
-        <button className='knapp'>-</button>
-        <button className='knapp'>.</button>
-        <button className='knapp'>0</button>
-        <button className='knapp'>/</button>
-        <button className='knapp'>x</button>
+      <div className='keypad'>
+        <button className='buttons'>7</button>
+        <button className='buttons'>8</button>
+        <button className='buttons'>9</button>
+        <button className='del'>DEL</button>
+        <button className='buttons'>4</button>
+        <button className='buttons'>5</button>
+        <button className='buttons'>6</button>
+        <button className='buttons'>+</button>
+        <button className='buttons'>1</button>
+        <button className='buttons'>2</button>
+        <button className='buttons'>3</button>
+        <button className='buttons'>-</button>
+        <button className='buttons'>.</button>
+        <button className='buttons'>0</button>
+        <button className='buttons'>/</button>
+        <button className='buttons'>x</button>
         <button className='reset'>RESET</button>
         <button className='equal'>=</button>
       </div>
